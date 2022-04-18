25bc9c84e39afd38f48aeb10f6890a59bec39152
Piyaponf <>
2022-04-18 17:06:13 +0700

    update icon

--- app/src/main/java/com/android/topscare/domain/data/remote/Api.kt
+++ app/src/main/java/com/android/topscare/domain/data/remote/Api.kt
@@ -8,45 +8,45 @@ import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface Api {
-    @GET("topscare/index.php/product/search")
+    @GET("worldMedic/index.php/product/search")
     suspend fun searchProduct(@Query("key") key : String): NetworkResponseWrapper<ProductResponse?>
 
-    @POST("topscare/index.php/product/count")
+    @POST("worldMedic/index.php/product/count")
     suspend fun addCountProduct(@Body countRequest : CountRequest): NetworkResponseWrapper<Any>
 
-    @POST("topscare/index.php/product/order")
+    @POST("worldMedic/index.php/product/order")
     suspend fun insertOrderProduct(@Body orderRequest : OrderRequest): NetworkResponseWrapper<Any>
 
-    @POST("topscare/index.php/product/receive")
+    @POST("worldMedic/index.php/product/receive")
     suspend fun insertReceiveProduct(@Body receiveRequest : ReceiveRequest): NetworkResponseWrapper<Any>
 
-    @GET("topscare/index.php/product/listCount")
+    @GET("worldMedic/index.php/product/listCount")
     suspend fun searchProductCount(@Query("limit") limit : Int,
                                    @Query("page") page : Int,
                                    @Query("key") key : String?
     ): NetworkResponseWrapper<List<CountResponse>?>
 
-    @POST("topscare/index.php/product/deleteCount")
+    @POST("worldMedic/index.php/product/deleteCount")
     suspend fun deleteCountProduct(@Body deleteRequest : DeleteRequest): NetworkResponseWrapper<Any>
 
-    @GET("topscare/index.php/product/listOrder")
+    @GET("worldMedic/index.php/product/listOrder")
     suspend fun searchProductOrder(@Query("limit") limit : Int,
                                    @Query("page") page : Int,
                                    @Query("key") key : String?
     ): NetworkResponseWrapper<List<OrderHistoryResponse>?>
 
-    @POST("topscare/index.php/product/deleteOrder")
+    @POST("worldMedic/index.php/product/deleteOrder")
     suspend fun deleteOrderProduct(@Body deleteRequest : DeleteRequest): NetworkResponseWrapper<Any>
 
-    @GET("topscare/index.php/product/listReceive")
+    @GET("worldMedic/index.php/product/listReceive")
     suspend fun searchProductReceive(@Query("limit") limit : Int,
                                    @Query("page") page : Int,
                                    @Query("key") key : String?
     ): NetworkResponseWrapper<List<ReceiveHistoryResponse>?>
 
-    @POST("topscare/index.php/product/deleteReceive")
+    @POST("worldMedic/index.php/product/deleteReceive")
     suspend fun deleteReceiveProduct(@Body deleteRequest : DeleteRequest): NetworkResponseWrapper<Any>
 
-    @POST("/topscare/setup/index.php/setting/check")
+    @POST("/worldMedic/setup/index.php/setting/check")
     suspend fun checkIp(@Body checkIpRequest : CheckIpRequest): NetworkResponseWrapper<Any>
 }
